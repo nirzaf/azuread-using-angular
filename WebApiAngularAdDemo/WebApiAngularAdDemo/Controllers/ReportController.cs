73f7a767724dcca0457da150cbc3934ca2da8a0b
Fazrin <mfmfazrin1986@gmail.com>
2022-07-06 21:53:54 +0300

    Azure Angular_WebAPI_AzureAD_Authentication_Authorization

--- WebApiAngularAdDemo/WebApiAngularAdDemo/Controllers/ReportController.cs
+++ WebApiAngularAdDemo/WebApiAngularAdDemo/Controllers/ReportController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApiAngularAdDemo.Controllers
 {
